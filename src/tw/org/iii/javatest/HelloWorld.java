310aad2a3206db5599f2c3e021badbf68e8aa3dd
Brad <brad@brad.tw>
2016-10-31 11:48:25 +0800

    2016-10-31 v1

---
+++ src/tw/org/iii/javatest/HelloWorld.java
@@ -0,0 +1,7 @@
+package tw.org.iii.javatest;
+public class HelloWorld {
+    public static void main(String[] brad){
+        System.out.println("Hello, Brad2");
+        //System.exit(0);
+    }
+}
