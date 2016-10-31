96701b481355353ab54a8ba5a8bb8d9df715eae2
Brad Chao <brad@brad.tw>
2016-10-31 14:27:37 +0800

    2016-10-31 v2

---
+++ src/tw/org/iii/javatest/Brad02.java
@@ -0,0 +1,12 @@
+package tw.org.iii.javatest;
+
+public class Brad02 {
+    public static void main(String[] args){
+        float Var1 = 12;
+        float Var2 = 0.1f;
+        double Var3 = 0, Var4 = 0;
+        int Var5 = 10, Var6 = 0;
+        System.out.println(Var3 / Var6);
+    }
+
+}
