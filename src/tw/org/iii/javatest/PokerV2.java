991a8a96a0dc6691f36d7cc80d2f4086d1a4f058
Brad Chao <brad@brad.tw>
2016-11-01 16:15:42 +0800

    2016-11-01 v8

--- src/tw/org/iii/javatest/PokerV2.java
+++ src/tw/org/iii/javatest/PokerV2.java
@@ -1,4 +1,7 @@
 package tw.org.iii.javatest;
+
+import java.util.Arrays;
+
 public class PokerV2 {
     public static void main(String[] args){
         // 洗牌
@@ -27,15 +30,19 @@ public class PokerV2 {
             players[i%4][i/4] = poker[i];
         }
 
+        // 攤牌 (理牌)
+        String[] suits = {"黑桃","紅心","方塊","梅花"};
+        String[] values = {"A ","2 ","3 ","4 ","5 ","6 ","7 "
+                ,"8 ","9 ","10","J ","Q ","K "};
         for (int[] player : players){
+            Arrays.sort(player);
             for (int card : player){
-                System.out.print(card + "  ");
+                System.out.print(suits[card/13] + values[card%13] + "  ");
             }
             System.out.println();
         }
 
 
 
-        // 攤牌 (理牌)
     }
 }
