f0e70ad6bd5ae723f1a8398c8116a60f4e863275
Brad Chao <brad@brad.tw>
2016-11-04 16:33:34 +0800

    2016-11-04 v9

--- src/tw/org/iii/javatest/GuessNumber.java
+++ src/tw/org/iii/javatest/GuessNumber.java
@@ -5,11 +5,12 @@ import javax.swing.*;
 public class GuessNumber {
     public static void main(String[] args){
         // 1. 產生謎底
-        String answer = createAnswer();
+        String answer = createAnswer(3);
+        //System.out.println(answer);
 
         // 2. 開始猜
         boolean isWINNER = false;
-        for (int i=0; i<3; i++) {
+        for (int i=0; i<10; i++) {
             String guess = JOptionPane.showInputDialog("輸入答案:");
             String result = checkAB(answer, guess);
             JOptionPane.showMessageDialog(null, result);
@@ -25,10 +26,40 @@ public class GuessNumber {
         }
 
     }
-    static String createAnswer(){
-        return "417";
+    static String createAnswer(int n){
+        // 洗牌
+        int[] poker = new int[n]; // poker[0] = 0, ....
+        int temp; boolean isRepeat;
+        for (int i=0; i<poker.length; i++){
+            do{
+                temp = (int)(Math.random()*10);
+                // 檢查機制
+                isRepeat = false;
+                for (int j=0; j<i; j++){
+                    if (poker[j] == temp){
+                        // 此時重複了
+                        isRepeat = true;
+                        break;
+                    }
+                }
+            }while (isRepeat);
+            poker[i] = temp;
+            //System.out.println(poker[i]);
+        }
+        String ret = "";
+        for (int v : poker) ret += v;
+
+        return ret;
     }
     static String checkAB(String a, String g){
-        return "3A0B";
+        int A, B; A = B = 0;
+        for (int i=0; i<a.length(); i++){
+            if (g.charAt(i) == a.charAt(i)){
+                A++;
+            }else if (a.indexOf(g.charAt(i)) != -1){
+                B++;
+            }
+        }
+        return A + "A" + B + "B";
     }
 }
