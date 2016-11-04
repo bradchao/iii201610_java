d530635bd49031dbfb53cdac38ca7a3282c60549
Brad Chao <brad@brad.tw>
2016-11-04 10:39:39 +0800

    2016-11-04 v3

---
+++ src/tw/org/iii/javatest/TWId.java
@@ -0,0 +1,23 @@
+package tw.org.iii.javatest;
+
+public class TWId {
+    private String id;
+
+    public boolean isCheckOK(){
+        // TODO --- isCheckOK()
+        return true;
+    }
+
+    public String getArea(){
+        // TODO --- getArea()
+        return "";
+    }
+    public boolean getGender(){
+        // TODO --- getGender()
+        return true;
+    }
+
+    public String getId(){
+        return id;
+    }
+}
