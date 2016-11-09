61d70ccb625f44bec737d2b9d1d8614e0618890d
Brad Chao <brad@brad.tw>
2016-11-09 10:42:36 +0800

    2016-11-09 v6

--- src/tw/org/iii/javatest/Brad42.java
+++ src/tw/org/iii/javatest/Brad42.java
@@ -1,17 +1,24 @@
 package tw.org.iii.javatest;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.logging.SocketHandler;
 
-/**
- * Created by user01 on 2016/11/8.
- */
+// Client
 public class Brad42 {
     public static void main(String[] args){
         try {
             Socket socket = new Socket(
-                    InetAddress.getByName(""), 9999);
+                    InetAddress.getByName("10.2.24.106"), 9999);
+
+            BufferedOutputStream bout =
+                    new BufferedOutputStream(
+                            socket.getOutputStream());
+            bout.write("Hello, Brad\nHello, World".getBytes());
+            bout.flush();
+            bout.close();
 
             socket.close();
             System.out.println("OK");
--- src/tw/org/iii/javatest/Brad43.java
+++ src/tw/org/iii/javatest/Brad43.java
@@ -1,14 +1,32 @@
 package tw.org.iii.javatest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
+// Server
 public class Brad43 {
     public static void main(String[] args){
         try {
             ServerSocket server =
                     new ServerSocket(9999);
-            server.accept();
+            System.out.println("Listening...");
+            Socket socket = server.accept();
+
+            String urip = socket.getInetAddress().getHostAddress();
+            System.out.println(urip + ":");
+            BufferedReader br =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    socket.getInputStream()));
+            String line;
+            while ( (line = br.readLine()) != null){
+                System.out.println(line);
+            }
+            br.close();
+
             server.close();
             System.out.println("Server OK");
         } catch (IOException e) {
