0379bf66222d6795d35597d4731acd262ab33a47
Brad Chao <brad@brad.tw>
2016-11-11 14:54:31 +0800

    2016-11-11 v5

--- src/tw/org/iii/javatest/MyPanel.java
+++ src/tw/org/iii/javatest/MyPanel.java
@@ -9,11 +9,12 @@ public class MyPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D)g;
 
-        g2d.setColor(Color.RED);
-        g2d.setStroke(new BasicStroke(4));
+        setBackground(Color.BLACK);
 
-        g2d.drawOval(100,100, 40, 40);
-        g2d.fillOval(200,200, 40, 40);
+        g2d.setColor(Color.YELLOW);
+        g2d.setStroke(new BasicStroke(2));
+        g2d.drawLine(0,0,300,300);
+
 
     }
 }
