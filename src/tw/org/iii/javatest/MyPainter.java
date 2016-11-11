f6c91a054a1a3b9e920350dea97b31d50208d29f
Brad Chao <brad@brad.tw>
2016-11-11 15:59:13 +0800

    2016-11-11 v6

--- src/tw/org/iii/javatest/MyPainter.java
+++ src/tw/org/iii/javatest/MyPainter.java
@@ -24,6 +24,7 @@ public class MyPainter extends JFrame {
         setSize(800, 600);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
     }
     public static void main(String[] args){
         new MyPainter();
--- src/tw/org/iii/javatest/MyPanel.java
+++ src/tw/org/iii/javatest/MyPanel.java
@@ -2,19 +2,27 @@ package tw.org.iii.javatest;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-public class MyPanel extends JPanel {
+public class MyPanel extends JPanel implements MouseMotionListener {
+    public MyPanel(){
+        addMouseMotionListener(this);
+    }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D)g;
-
         setBackground(Color.BLACK);
-
-        g2d.setColor(Color.YELLOW);
-        g2d.setStroke(new BasicStroke(2));
-        g2d.drawLine(0,0,300,300);
-
-
     }
+
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        int x = e.getX(); int y = e.getY();
+        System.out.println(x + " x " + y);
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {}
 }
