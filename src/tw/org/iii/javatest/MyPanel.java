ac5446e269fdfdd5bfffb92ff549d4fbd9524824
Brad Chao <brad@brad.tw>
2016-11-14 09:47:24 +0800

    2016-11-14 v1

--- src/tw/org/iii/javatest/MyPanel.java
+++ src/tw/org/iii/javatest/MyPanel.java
@@ -9,14 +9,13 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MyPanel extends JPanel {
-    private LinkedList<HashMap<String,Integer>> line;
-    private MyMouseListener myMouseListener;
+    private LinkedList<LinkedList<HashMap<String,Integer>>> lines = null;
 
     public MyPanel(){
-        myMouseListener = new MyMouseListener();
+        MyMouseListener myMouseListener = new MyMouseListener();
         addMouseMotionListener(myMouseListener);
         addMouseListener(myMouseListener);
-        line = new LinkedList<>();
+        lines = new LinkedList<>();
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -26,27 +25,41 @@ public class MyPanel extends JPanel {
         g2d.setColor(Color.YELLOW);
         g2d.setStroke(new BasicStroke(2));
 
-        // line(4) => 0-1, 1-2, 2-3
-        for (int i=1; i<line.size(); i++){
-            HashMap<String,Integer> p0 = line.get(i-1);
-            HashMap<String,Integer> p1 = line.get(i);
-            g2d.drawLine(p0.get("x"), p0.get("y"),
-                    p1.get("x"), p1.get("y"));
+        for (LinkedList<HashMap<String,Integer>> line : lines){
+            // line(4) => 0-1, 1-2, 2-3
+            for (int i=1; i<line.size(); i++){
+                HashMap<String,Integer> p0 = line.get(i-1);
+                HashMap<String,Integer> p1 = line.get(i);
+                g2d.drawLine(p0.get("x"), p0.get("y"),
+                        p1.get("x"), p1.get("y"));
+            }
         }
+
+
 
 
     }
 
     private class MyMouseListener extends MouseAdapter {
         @Override
-        public void mouseDragged(MouseEvent e) {
+        public void mousePressed(MouseEvent e) {
+            LinkedList<HashMap<String,Integer>> line = new LinkedList<>();
             int x = e.getX(); int y = e.getY();
             HashMap<String,Integer> point = new HashMap<>();
             point.put("x", x); point.put("y", y);
             line.add(point);
+            lines.add(line);
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            int x = e.getX(); int y = e.getY();
+            HashMap<String,Integer> point = new HashMap<>();
+            point.put("x", x); point.put("y", y);
+            lines.getLast().add(point);
             repaint();
         }
-        
+
 
     }
 }
